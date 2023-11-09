5432040e702391a7dcf4ff2044649519a7d42177
Nayfall <nayfall243@gmail.com>
2023-11-09 14:06:19 -0300

    Changed migration tech name

--- migrations/0.4.2.lua
+++ migrations/0.4.2.lua
@@ -1,7 +1,7 @@
 -- Change file name to enable migrations! (Took me 2 days to find this out)
 
 for index, force in pairs(game.forces) do
-	if force.technologies["armored-train-turret-wagons"].researched then
+	if force.technologies["armored-train-turret-wagons-MK1"].researched then
 
 		force.recipes["armored-locomotive-mk1"].enabled = true
 		force.recipes["minigun-platform-mk1"].enabled = true
